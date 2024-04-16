fb940f21d21ad20ef27e0dee64c8418661975474
AmeyaBhave28UMBC <AMEYAB1@UMBC.EDU>
2024-04-16 11:58:17 -0400

    Maps implementations Complete
    Maps implementations Complete

---
+++ maps/src/CustomMap.ts
@@ -0,0 +1,47 @@
+// Intsructions to every other class
+// on how they can be an argument to 'addMarker'
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(
+      document.getElementById(divId) as HTMLElement,
+      {
+        zoom: 1,
+        center: {
+          lat: 0,
+          lng: 0,
+        },
+        mapId: 'DEMO_MAP_ID',
+      }
+    );
+  }
+
+  async addMarker(mappable: Mappable): Promise<void> {
+    const { AdvancedMarkerElement } = (await google.maps.importLibrary(
+      'marker'
+    )) as google.maps.MarkerLibrary;
+    const marker = new AdvancedMarkerElement({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
